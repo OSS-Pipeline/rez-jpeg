81ab1ffde169cb2a680ec2480841cf7f29490f1b
Joshua Senouf <jsenouf@MTLws032.mtloutpost.com>
2019-10-08 14:23:15 -0400

    Adding support for PKG_CONFIG_PATH

--- package.py
+++ package.py
@@ -39,6 +39,7 @@ uuid = "jpeg-{version}".format(version=str(version))
 def commands():
     env.PATH.prepend("{root}/bin")
     env.LD_LIBRARY_PATH.prepend("{root}/lib")
+    env.PKG_CONFIG_PATH.prepend("{root}/lib/pkgconfig")
 
     # Helper environment variables.
     env.JPEG_BINARY_PATH.set("{root}/bin")
